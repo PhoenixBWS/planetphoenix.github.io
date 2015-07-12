cc1f428b012746d3a8f14712d1ec2b7cf67d411a
PlanetPhoenix <goldi.sen@gmail.com>
2015-07-12 18:04:30 +0530

    Update NGine.js

--- js/NGine.js
+++ js/NGine.js
@@ -1,7 +1,15 @@
 var day = new Date().getDay();
-var colours = ['#D4C81E', '#1ECED4', '#F38080', '#F67BC0', '#CC7BF6', '#73C09D', '#C0A473'];
+var colours = ['#D4C81E', '#1ECED4'];
+var val;
+// var colours = ['#D4C81E', '#1ECED4', '#F38080', '#F67BC0', '#CC7BF6', '#73C09D', '#C0A473'];
+
+if(day%2 == 0){
+	val = 0
+} else{
+	val = 1
+}
 
 $(document).ready(function() {
-    $('body').css('background-color', colours[day]);
-	$('header').css('color', colours[day]);
+    $('body').css('background-color', colours[val]);
+	$('header').css('color', colours[val]);
 });
